d5e3459295f80f8e9184a39c48d4d3659cbbf7c7
Boris M <boris.milojevic13@gmail.com>
2022-01-27 11:45:16 +0100

    samo lista unazad - popraviti

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let index = 0;
+
+let show_slide = function(i){
+    index+=i;
+
+const images = document.getElementsByClassName('image');
+
+const dots = document.getElementsByClassName('dot');
+
+for(let i = 0; i < images.length; i++){
+    images[i].style.display = 'none';
+}
+
+/* wraparound */
+if(index > (images.length-1)){
+    index = 0;
+}
+
+if (index<0){
+    index = images.length - 1;
+}
+/* wraparound */
+
+/* zamjenjuje display: none samo na slici na kojoj smo sad
+display: block efektivno koristimo kao roundabout way 
+prikazivanja slike  */
+images[index].style.display = "block";
+
+for (i = 0; i<dots.length; i++){
+    dots[i].className = dots[i].className.replace("active","");
+}
+
+dots[index].className += " active";
+};
+window.addEventListener("onload", show_slide(index));
